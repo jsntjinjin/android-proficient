698df5ad935a80aa0858e8b4f21eca7f7e8ae3a8
jinjin <313919790@qq.com>
2017-07-05 17:12:45 +0800

    添加垂直顺滑菜单

--- app/src/main/java/com/fastaoe/proficient/component/views/VerticalDragActivity.java
+++ app/src/main/java/com/fastaoe/proficient/component/views/VerticalDragActivity.java
@@ -1,8 +1,22 @@
 package com.fastaoe.proficient.component.views;
 
+import android.support.v7.widget.LinearLayoutManager;
+
+import com.fastaoe.baselibrary.ioc.Bind;
 import com.fastaoe.baselibrary.ioc.ContentView;
+import com.fastaoe.baselibrary.utils.LogUtil;
 import com.fastaoe.framelibrary.BaseSkinActivity;
 import com.fastaoe.proficient.R;
+import com.fastaoe.proficient.component.recycler.HeadFooterRecyclerActivity;
+import com.fastaoe.proficient.weight.recycler.LinearLayoutItemDecoration;
+import com.fastaoe.proficient.weight.recycler.base.RecyclerAdapter;
+import com.fastaoe.proficient.weight.recycler.base.ViewHolder;
+import com.fastaoe.proficient.weight.recycler.wrap.WrapRecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.fastaoe.proficient.R.id.recycler_view;
 
 /**
  * Created by jinjin on 2017/7/4.
@@ -11,6 +25,10 @@ import com.fastaoe.proficient.R;
 
 @ContentView(R.layout.activity_view_vertical_drag)
 public class VerticalDragActivity extends BaseSkinActivity {
+
+    @Bind(R.id.wrcv)
+    WrapRecyclerView wrcv;
+
     @Override
     protected void initTitle() {
 
@@ -23,6 +41,50 @@ public class VerticalDragActivity extends BaseSkinActivity {
 
     @Override
     protected void initData() {
+        List<ChatItem> list = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            if (i % 3 == 0) {
+                list.add(new ChatItem("第" + i + "个item", 0));
+            } else {
+                list.add(new ChatItem("第" + i + "个item", 1));
+            }
+        }
 
+        wrcv.setLayoutManager(new LinearLayoutManager(this));
+        wrcv.setAdapter(initRecyclerAdapter(list));
+        wrcv.addItemDecoration(new LinearLayoutItemDecoration(this, R.drawable.item_dirver_01));
+
+    }
+
+    private RecyclerAdapter initRecyclerAdapter(List<ChatItem> recyclerData) {
+        RecyclerAdapter adapter = new RecyclerAdapter<ChatItem>(this, recyclerData, item -> {
+            ChatItem chatItem = (ChatItem) item;
+            if (chatItem.type == 0) {
+                return R.layout.item_chat_01;
+            }
+            return R.layout.item_chat_02;
+        }) {
+            @Override
+            protected void convert(ViewHolder holder, ChatItem data, int position) {
+                if (data.type == 0) {
+                    holder.setText(R.id.item_me, data.content);
+                } else {
+                    holder.setText(R.id.item_friend, data.content);
+                }
+            }
+        };
+
+        adapter.setOnItemClickListener(position -> LogUtil.d("TAG", "点击子条目 -> " + recyclerData.get(position)));
+        return adapter;
+    }
+
+    public class ChatItem {
+        public String content;
+        public int type;
+
+        public ChatItem(String content, int type) {
+            this.content = content;
+            this.type = type;
+        }
     }
 }
--- app/src/main/java/com/fastaoe/proficient/component/views/weight/VerticalDragView.java
+++ app/src/main/java/com/fastaoe/proficient/component/views/weight/VerticalDragView.java
@@ -4,10 +4,12 @@ import android.content.Context;
 import android.support.annotation.AttrRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.FrameLayout;
 
 /**
@@ -18,6 +20,10 @@ import android.widget.FrameLayout;
 public class VerticalDragView extends FrameLayout {
 
     private ViewDragHelper helper;
+
+    private View viewDragListView;
+    private int menuHeight;
+    private boolean isMenuOpen = false;
 
     public VerticalDragView(@NonNull Context context) {
         this(context, null);
@@ -30,12 +36,46 @@ public class VerticalDragView extends FrameLayout {
     public VerticalDragView(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
-        helper = ViewDragHelper.create(this, mViewDragHelper);
+        helper = ViewDragHelper.create(this, mViewDragHelperCallback);
     }
 
     @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        int childCount = getChildCount();
+        if (childCount != 2) {
+            throw new RuntimeException("child views should be only two !");
+        }
+
+        viewDragListView = getChildAt(1);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        menuHeight = getChildAt(0).getMeasuredHeight();
+    }
+
+    private float currentY = 0;
+    @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        return helper.shouldInterceptTouchEvent(ev);
+        if (isMenuOpen) {
+            return true;
+        }
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                currentY = ev.getY();
+                helper.processTouchEvent(ev);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (currentY < ev.getY() && !canChildScrollUp(viewDragListView)) {
+                    return true;
+                }
+                break;
+        }
+
+        return super.onInterceptTouchEvent(ev);
     }
 
     @Override
@@ -44,15 +84,63 @@ public class VerticalDragView extends FrameLayout {
         return true;
     }
 
-    private ViewDragHelper.Callback mViewDragHelper = new ViewDragHelper.Callback() {
+    @Override
+    public void computeScroll() {
+        if (helper.continueSettling(true)) {
+            invalidate();
+        }
+    }
+
+    private ViewDragHelper.Callback mViewDragHelperCallback = new ViewDragHelper.Callback() {
         @Override
         public boolean tryCaptureView(View child, int pointerId) {
-            return false;
+            return child == viewDragListView;
         }
 
         @Override
         public int clampViewPositionVertical(View child, int top, int dy) {
+            if (top < 0) {
+                top = 0;
+            }
+
+            if (top >= menuHeight) {
+                top = menuHeight;
+            }
+
             return top;
         }
+
+        @Override
+        public void onViewReleased(View releasedChild, float xvel, float yvel) {
+            if (viewDragListView.getTop() > menuHeight / 2) {
+                // open
+                helper.settleCapturedViewAt(0, menuHeight);
+                isMenuOpen = true;
+            } else {
+                // close
+                helper.settleCapturedViewAt(0, 0);
+                isMenuOpen = false;
+            }
+            invalidate();
+        }
     };
+
+    /**
+     * @return Whether it is possible for the child view of this layout to
+     *         scroll up. Override this if the child view is a custom view.
+     */
+    public boolean canChildScrollUp(View view) {
+        if (android.os.Build.VERSION.SDK_INT < 14) {
+            if (view instanceof AbsListView) {
+                final AbsListView absListView = (AbsListView) view;
+                return absListView.getChildCount() > 0
+                        && (absListView.getFirstVisiblePosition() > 0 || absListView.getChildAt(0)
+                        .getTop() < absListView.getPaddingTop());
+            } else {
+                return ViewCompat.canScrollVertically(view, -1) || view.getScrollY() > 0;
+            }
+        } else {
+            return ViewCompat.canScrollVertically(view, -1);
+        }
+    }
 }
